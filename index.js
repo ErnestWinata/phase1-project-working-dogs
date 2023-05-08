26a1d11a31a32521c2340370967cdbcfe9f5a128
Ernest <ernestwinata@gmail.com>
2023-05-08 07:40:11 -0700

    select only working group from thedogapi.com

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ fetch(
   "https://api.thedogapi.com/v1/breeds?"
 )
   .then((response) => response.json())
-  
+  .then((data) => {
+    const workingDogs = data
+      .filter((dog) => dog.breed_group === "Working") 
+      .slice(0, 3) 
+      
