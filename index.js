8245e8d1291610bf6340f615875015899bb6afda
Ernest <ernestwinata@gmail.com>
2023-05-08 07:43:55 -0700

    put each breed into a card

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ fetch(
       .filter((dog) => dog.breed_group === "Working") 
       .slice(0, 3) 
       .concat(data.filter((dog) => dog.breed_group === "Working").slice(5, 8));
+
+      console.log(workingDogs);
+    console.log(workingDogs);
+    workingDogs.forEach((dog, index) => {
+      const div = document.createElement("div");
+      div.classList.add("dog-container");
