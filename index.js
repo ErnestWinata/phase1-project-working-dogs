a20c2a9fb67141f2fcff87226e191674d93db470
Ernest <ernestwinata@gmail.com>
2023-05-08 07:44:27 -0700

    add a cute thumbnail on top of each card

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ fetch(
     workingDogs.forEach((dog, index) => {
       const div = document.createElement("div");
       div.classList.add("dog-container");
+
+      const img = document.createElement("img");
+      img.src =
+        "https://media.istockphoto.com/id/1155747156/vector/cute-golden-retriever-puppy-paws-up-over-wall-dog-face-vector-illustration.jpg?s=612x612&w=0&k=20&c=4ROMUd2EJsRxPKh8EdNjEGioQtt1tPYza_bzmUhwaSM=";
+      img.alt = `${dog.name} image`;
